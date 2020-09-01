b0eab35160be8a5192cfbb0b72803a8497da457b
dread <dread@DESKTOP-BS1AKPM>
2020-08-31 19:52:11 -0700

    this is a comment message

---
+++ Part2/Program.cs
@@ -0,0 +1,36 @@
+﻿/// Chapter No. 1		Exercise No. 1
+/// File Name: Part 2
+/// @author: Dakota Durst
+/// Date:  August 31, 2020
+///
+/// Problem Statement: USe console.writeline to create a shape
+/// 
+/// 
+/// Overall Plan:
+/// 1) use write line method to draw a shape on the screen
+/// 2) 
+/// 3) 
+/// 4) 
+/// 
+using System;
+
+namespace part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("                  ____        ___                       ");
+            Console.WriteLine("                /     )~~~~~~(    )                     ");
+            Console.WriteLine("               /                   )                ");
+            Console.WriteLine("          ____|           .  .      )           ");
+            Console.WriteLine("        /     |           __       /         ");
+            Console.WriteLine("       /      <_______       _____/             ");
+            Console.WriteLine("      /                ((    |(                  ");
+            Console.WriteLine("     / |    (   )     __/(   |                   ");
+            Console.WriteLine("    ^  |   | ____|   |    ( |                     ");
+            Console.WriteLine("       |___|     |___|     ''                     ");
+            Console.WriteLine("              its an elephant!                                    ");
+        }
+    }
+}
