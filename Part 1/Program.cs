a486d177d3bdc66dcf3e5d332ffea965d0c89a80
dread <dread@DESKTOP-BS1AKPM>
2020-08-31 19:55:36 -0700

    updating part 1

--- Part 1/Program.cs
+++ Part 1/Program.cs
@@ -6,7 +6,7 @@ namespace Part_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("removed");
         }
     }
 }
